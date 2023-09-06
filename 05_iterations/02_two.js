4a7f16d79336a6ed328826e88a7b8c44af812006
Nikhil <ace.nikchauhan@gmail.com>
2023-09-06 23:13:32 +0530

    while loop added

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,30 @@
+// while loop syntax
+
+// while (condition) {
+
+// }
+
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2;
+// }
+
+// let myArr = ["thor", "flash", "spiderman"];
+// let arr = 0;
+// while (arr < myArr.length) {
+//   console.log(`Value is ${myArr[arr]}`);
+//   arr = arr + 1;
+// }
+
+// do while loop syntax
+
+// do {
+
+// } while (condition);
+
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
