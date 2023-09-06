b39be2fa21513608c687e59e1fe0ca0b4306ae1f
Nikhil <ace.nikchauhan@gmail.com>
2023-09-06 23:37:34 +0530

    for in loop added

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,37 @@
+const myObj = {
+  js: "JavaScript",
+  cpp: "C++",
+  rb: "ruby",
+  swift: "swift by apple",
+};
+
+//for in loop
+
+// for (const key in object) {
+
+// }
+
+// for (const key in myObj) {
+//   //   console.log(key);
+//   //   console.log(myObj[key]);
+//   console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+// const programming = ["js", "java", "python", "cpp"];
+
+// for (const key in programming) {
+//   console.log(key);
+// }
+
+//maps
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("AUS", "Australia");
+// map.set("Fr", "France");
+// map.set("RUS", "Russia");
+// map.set("IN", "India"); // no duplication allowed
+// // maps are not iteratable so we can't get the values like this
+// // there are different ways to get the value and key of map
+// for (const key in map) {
+//   console.log(key);
+// }
