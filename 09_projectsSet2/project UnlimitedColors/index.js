4297cce61b11a47d190f79f5e165b30c17c0a1d7
Nikhil <ace.nikchauhan@gmail.com>
2023-09-14 19:25:52 +0530

    random color project

---
+++ 09_projectsSet2/project UnlimitedColors/index.js
@@ -0,0 +1,32 @@
+//generate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+// console.log(randomColor());
+let intervalID;
+const startChangingColor = () => {
+  if (!intervalID) {
+    intervalID = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+const stopChangingColor = () => {
+  clearInterval(intervalID);
+  intervalID = null;
+  console.log("Stopped");
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
